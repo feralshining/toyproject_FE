528c25ed9054ca9b1ed1d574cca5aef54cd25da6
feralshining <racc00ns@naver.com>
2026-01-13 23:30:20 +0900

    feat: 네비게이션 바 드롭다운 메뉴 추가 및 스타일 수정 - 네비게이션 바에 드롭다운 메뉴 기능 추가

--- src/components/layout/header/navigationBar/NavigationBar.tsx
+++ src/components/layout/header/navigationBar/NavigationBar.tsx
@@ -1,13 +1,27 @@
+import React from 'react';
 import '/src/styles/Fonts.css';
 import './NavigationBar.css';
 
 function NavigationBar() {
+    const [isOpen, setIsOpen] = React.useState(false);
     return (
         <div className='navigationBar-container'>
             <button className='nav-button'>공지사항</button>
-            <div className='nav-r'>
-                <button className='nav-button'>게시판</button>
-                <img src="dropdown-arrow.png" className='dropdown-button'/>
+            <div className='nav-dropdown-container'>
+                <button 
+                    className='nav-button dropdown-trigger'
+                    onClick={() => setIsOpen(!isOpen)}
+                >
+                    게시판
+                    <img className='dropdown-arrow' src="dropdown-arrow.png" />
+                </button>
+                
+                <ul className={`dropdown-item ${isOpen ? 'show' : ''}`}>
+                    <li><a href="#">인기 게시판</a></li>
+                    <li><a href="#">자유 게시판</a></li>
+                    <li><a href="#">질문 게시판</a></li>
+                    <li><a href="#">자료실</a></li>
+                </ul>
             </div>
             <button className='nav-button'>상점</button>
             <button className='nav-button'>고객센터</button>
